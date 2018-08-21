6dd6557557220556387a155e2c1d080011c0ba76
daijunjian <daijunjian11@gmail.com>
2018-08-21 15:43:48 +0800

    试用 react-native-timeline-listview （效果不理想）
    Change-Id: Icc440b9d5893b36c7ac58311da51aa4590632a01

--- src/components/sub_info/InfoMainPage.js
+++ src/components/sub_info/InfoMainPage.js
@@ -1,12 +1,35 @@
 import React from 'react'
 import {
-    Text
+    Text,
+    View,
+    StyleSheet
 } from 'react-native'
+import Timeline from 'react-native-timeline-listview'
 
 export default class InfoMainPage extends React.Component {
+
+    constructor(props){
+        super(props)
+        this.data = [
+            {time: '09:00', title: 'Event 1', description: 'Event 1 Description'},
+            {time: '10:45', title: 'Event 2', description: 'Event 2 Description'},
+            {time: '12:00', title: 'Event 3', description: 'Event 3 Description'},
+            {time: '14:00', title: 'Event 4', description: 'Event 4 Description'},
+            {time: '16:30', title: 'Event 5', description: 'Event 5 Description'}
+        ]
+    }
     render(){
         return (
-            <Text>我的信息</Text>
+                <Timeline
+                    data={this.data}
+                />
+
         )
     }
 }
+
+const styles = StyleSheet.create({
+    timeLineContainer: {
+        marginTop: -10
+    }
+})
