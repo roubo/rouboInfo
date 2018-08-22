9f43ff4d8492223d757452fefaad4bd9076d86d0
daijunjian <daijunjian11@gmail.com>
2018-08-22 09:19:31 +0800

    修改ios版本号
    Change-Id: Ibfdd14d9efa6fa3238e9c5a05e73488a610da229

--- src/components/sub_info/InfoMainPage.js
+++ src/components/sub_info/InfoMainPage.js
@@ -4,26 +4,24 @@ import {
     View,
     StyleSheet
 } from 'react-native'
-import Timeline from 'react-native-timeline-listview'
+import codePush from 'react-native-code-push'
 
 export default class InfoMainPage extends React.Component {
 
+
+    /**
+     * 应用中首次加载的组件，故在此次检测热更新
+     */
+    componentDidMount(){
+        codePush.sync()
+    }
+
     constructor(props){
         super(props)
-        this.data = [
-            {time: '09:00', title: 'Event 1', description: 'Event 1 Description'},
-            {time: '10:45', title: 'Event 2', description: 'Event 2 Description'},
-            {time: '12:00', title: 'Event 3', description: 'Event 3 Description'},
-            {time: '14:00', title: 'Event 4', description: 'Event 4 Description'},
-            {time: '16:30', title: 'Event 5', description: 'Event 5 Description'}
-        ]
     }
     render(){
         return (
-                <Timeline
-                    data={this.data}
-                />
-
+            <Text>暂无消息</Text>
         )
     }
 }
